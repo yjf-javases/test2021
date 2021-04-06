7af5977286a774de4780d0ca225e908af04909e0
yjf-global <2920594635@qq.com>
2021-04-06 15:00:10 +0800

    2021-04-06 第一次提交到远程仓库

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String name ;
+    private String email ;
+
+    public Student() {
+    }
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
